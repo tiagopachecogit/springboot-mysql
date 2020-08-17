7b2dbf133e6e04cec775d59ae14284f379057d5c
Tiago Pacheco <tiago-pacheco@hotmail.com>
2020-08-17 15:33:00 -0400

    primeiro commit

---
+++ src/test/java/com/tiago/springbootmysql/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.springbootmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
